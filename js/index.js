23074cb72a93fdc610535895c777ebb57c059103
Desiree Zalm <82588366+desireezalm@users.noreply.github.com>
2021-06-01 16:45:07 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,38 @@
+console.log("Hello Winc Academy");
+let name = 'Desiree';
+console.log('Desiree')
+//let if = 'gevolg'; > if is een keyword en kan daarom niet gebruikt worden.
+4 + 4;
+console.log(4 + 4)
+"4" + "4";
+console.log("4" + "4")
+
+
+//Addition +
+5 + 5;
+console.log(5 + 5)
+
+//Subtraction -
+5 - 4;
+console.log(5 - 4)
+
+//Multiplication *
+5 * 5;
+console.log(5 * 5)
+
+//Division /
+5 / 5;
+console.log(5 / 5)
+
+//Modulus %
+5 % 5;
+console.log(5 % 5)
+
+let leeftijd = 38;
+console.log(typeof 38)
+
+let leeftijd = "38";
+console.log(typeof "38")
+
+
+
